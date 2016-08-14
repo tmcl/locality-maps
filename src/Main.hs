227a9ef3cb3f016c4a429bcb289ebd1ecadfced9
Tristan McLeay <cassowary@fastmail.fm>
2016-08-14 20:52:19 +1000

    rewrite using conduits

--- src/Main.hs
+++ src/Main.hs
@@ -1,216 +1,53 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main
 where
 
--- import Turtle
-import System.Environment
-import System.Process
-import Data.Csv
-import Data.Maybe
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.Text as T
-import qualified Data.Set as S
-
-import qualified Data.Conduit.Shapefile as Sh
-
-import Geometry.Shapefile.MergeShpDbf
-import Geometry.Shapefile.Types
-
+import Geometry.Shapefile.Conduit
+import Data.Dbase.Conduit
 import Map
-import Municipality hiding (fullname)
-import Victoria
-import SouthAustralia
-import Queensland
-import Shapefile2
+import qualified Data.ByteString as BS
+import System.Environment
+import qualified Data.Conduit.Combinators as CC
+import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Binary as CB
+import Geometry.Shapefile.Types
+import Data.Conduit
+import Control.Monad.Trans.Resource
+import Control.Monad.IO.Class
+import Data.Maybe
+import System.IO
+import ClassyPrelude (trace,traceM)
+import System.FilePath
 
-import Control.Monad
+main = getArgs >>= mapM_ (mapFile)
 
-tshow :: Show z => z -> T.Text
-tshow = T.pack . show
+mapFile fp = mapFilePath fp "/tmp/blue.eps"
 
-j :: T.Text -> T.Text
-j = id
+mapFilePath src dst = do
+   withFile src ReadMode $ \srcH -> 
+      withFile dst WriteMode $ \dstH ->
+      mapFileHandle srcH dstH
 
-safeHead :: [a] -> Maybe a
-safeHead [] = Nothing
-safeHead (a:_) = Just a
+--mapFile :: FilePath -> ResourceT IO ()
+mapFileHandle src dst = do
+   Just (shpHead, _) <- CC.sourceHandle src =$= shapefileConduit $$ CL.head
+   let settings = withDefaultSettings (toRecBB $ shpBB shpHead)
+   initialisation <- liftIO $ initialiseMap settings
+   coast <- liftIO $ mapCoast settings
+   mapPoints <- (CB.sourceHandleRange src (Just 0) Nothing =$= shapefileConduit =$= points =$= CC.map (\f -> f)) =$= mapPoints3 settings (Outline (Points 1) (Color 0 100 200)) 5  $$ CC.sinkList
+   finalisation <- liftIO $ closeMap settings
+   CC.yieldMany ([initialisation] ++ mapPoints ++ [finalisation]) $$ CC.sinkHandle dst
 
-main :: IO ()
-main = do
-   args <- getArgs
-   print args
-   let state = (read . head) args :: State
-   print state
-   case state of
-     Vic -> mapM_ (return . it . MuniVic) allVictorian
-     Qld -> mapM_ (it . MuniQld) allQueensland
+pointsFromRecord rec = concatMap (pointsFromRecContents) (catMaybes [shpRecContents rec])
+pointsFromRecContents rec@(RecPolygon _ _ _ _ _) = recPolPoints rec
+pointsFromRecContents _ = []
 
+points = CC.map (pointsFromRecord . snd)
 
-doVictorian = do
-   let municipalities = map MuniVic allVictorian
-   mapM it municipalities
+toRecBB shpBB = RecBBox {
+      recXMin = shpXMin shpBB, 
+      recXMax = shpXMax shpBB, 
+      recYMin = shpYMin shpBB, 
+      recYMax = shpYMax shpBB 
+   }
 
-getLocalitiesByMunicipality :: MunicipalityInState -> IO (S.Set T.Text)
-getLocalitiesByMunicipality muni = do
-   vic' <- getVictorianLocalities
-   let vic = getRightOrElse mzero vic'
-   sa <- getSouthAustralianLocalities "/home/cassowary/Projects/map/source-data/sa/Gazetteer_shp/GazetteerSites.dbf"
-   qld <- getQueenslandLocalities "/home/cassowary/Projects/map/source-data/qld/QSC_Extracted_Data_20160801_222211270000-15668/Place_names_gazetteer.shp"
-
-   --let muni = readByState state municipality
-   return $ filterLocalities' muni (Localities vic sa qld)
-
---it :: State -> String -> IO ()
-it muni = do
-   localities <- getLocalitiesByMunicipality muni
-   case S.size localities of
-      0 -> error . show $ muni
-      _ -> print localities
-   maybeMapMatters' muni localities
-
-getRightOrElse :: a -> Either b a -> a
-getRightOrElse a (Left _) = a
-getRightOrElse _ (Right a) = a
-
-colorMajorUrban :: Pen
-colorMajorUrban = Solid (Color 100 100 100)
-colorOtherUrban :: Pen
-colorOtherUrban = Solid (Color 125 125 125)
-colorBoundedLocality :: Pen
-colorBoundedLocality = Solid (Color 150 150 150)
-
-colorTheLocality :: Pen
-colorTheLocality = Solid (Color 100 0 0)
-
-colorAllLocalities :: Pen
-colorAllLocalities = Outline (Points 0.4) (Color 100 0 0)
-
-colorAllMunicipalities :: Pen
-colorAllMunicipalities = Outline (Points 1.0) (Color 150 150 150)
-
-colorTheMunicipality :: Pen
-colorTheMunicipality = Solid (Color 254 254 233)
-
-maybeMapMatters' :: Foldable f => MunicipalityInState -> f T.Text -> IO ()
-maybeMapMatters' (MuniVic m) = maybeMapMatters m
-maybeMapMatters' (MuniQld m) = maybeMapMatters m
-maybeMapMatters' (MuniSA  m) = maybeMapMatters m
-
-isLocalityByName :: T.Text -> (T.Text, String) -> Bool
-isLocalityByName loc (field, locality)
-    = "_LOCA_2" `T.isSuffixOf` field && (T.strip . T.toUpper $ (T.takeWhile (\c -> c /= '(') loc)) == (T.strip . T.pack $ locality)
-
-allRecsInShapes :: [ShpData] -> [ShpRec]
-allRecsInShapes = concatMap shpRecs
-
-maybeMapMatters :: (Municipality m, Foldable f) => m -> f T.Text -> IO ()
-maybeMapMatters municipality localities = do
-    stateMunicipalityShapes <- readShpWithDbf (municipalityShapePath municipality)
-    let theMunicipalityRecs = shpRecByField (isMunicipalityByName municipality) stateMunicipalityShapes
-    let bboxes = catMaybes [catBoundingBoxes $ map Shapefile2.boundingBox theMunicipalityRecs]
-
-    mapM_ (\bbox -> mapMatters bbox municipality theMunicipalityRecs localities) (bboxes)
-
-outputEPSStreamToPDF :: Prelude.FilePath -> T.Text -> IO ()
-outputEPSStreamToPDF outfile stream = do
-   _ <- inproc "epstopdf" ["-f", T.pack $ "-o=" ++ outfile] stream
-   return ()
-
-mapLocality :: Municipality m => m -> T.Text -> Settings -> [ShpData] -> T.Text -> IO ()
-mapLocality m base settings allLocalityShapes locality = do
-    let theLocalityRecs = concatMap (shpRecByField (isLocalityByName locality)) allLocalityShapes
-    localityMap <- mapPoints settings (colorTheLocality, 60, (concatMap getPoints theLocalityRecs))
-    mapClosing <- closeMap settings
-    outputEPSStreamToPDF ("out/" ++ T.unpack locality ++ " - " ++ show m ++ ".pdf") (T.concat [base, localityMap, mapClosing])
-
-
-mapMatters :: (Municipality m, Foldable f) => RecBBox -> m -> [ShpRec] -> f T.Text -> IO ()
-mapMatters bbox municipality theMunicipalityRecs localities = do
-    allLocalityShapes <- allLocalities
-    let settings = withDefaultSettings bbox
-    base <- makeBaseMap settings allLocalityShapes theMunicipalityRecs 
-    mapClosing <- closeMap settings
-    outputEPSStreamToPDF ("out/" ++ show municipality ++ ".pdf") (T.concat [base, mapClosing])
-    mapM_ (mapLocality municipality base settings allLocalityShapes) localities
-
-makeBaseMap :: Settings -> [ShpData] -> [ShpRec] -> IO T.Text
-makeBaseMap settings allLocalityShapes theMunicipalityRecs = do
-    allMunicipalityShapes <- allMunicipalities
-    let allMunicipalityRecs = allRecsInShapes allMunicipalityShapes
-    (majorUrban, otherUrban, boundedLocality) <- fmap pickUrbans allUrbans
-    let localityPoints = concatMap getPoints (concatMap shpRecs allLocalityShapes)
-    fmap T.concat $ mapM id [ initialiseMap settings ,
-               mapCoast settings ,
-               mapPoints settings (colorTheMunicipality, 0, (concatMap getPoints theMunicipalityRecs)) ,
-               mapPoints settings (colorAllMunicipalities, 0, (concatMap getPoints allMunicipalityRecs)) ,
-               mapPoints settings (colorAllLocalities, 0, localityPoints) ,
-               mapPoints settings (colorMajorUrban, 60, (concatMap getPoints majorUrban)) ,
-               mapPoints settings (colorOtherUrban, 60, (concatMap getPoints otherUrban)) ,
-               mapPoints settings (colorBoundedLocality, 60, (concatMap getPoints boundedLocality)) ]
-    -- return $ T.concat thing
-
-municipalityShapePath :: Municipality m => m -> Prelude.FilePath
-municipalityShapePath = stateShapePath . municipalityState
-
-stateShapePath :: State -> Prelude.FilePath
-stateShapePath Qld = "/home/cassowary/Projects/map/source-data/cth/QLDLGAPOLYGON/QLD_LGA_POLYGON_shp.shp"
-stateShapePath Vic = "/home/cassowary/Projects/map/source-data/cth/VICLGAPOLYGON/VIC_LGA_POLYGON_shp.shp"
-stateShapePath SA  = "/home/cassowary/Projects/map/source-data/cth/SALGAPOLYGON/SA_LGA_POLYGON_shp.shp"
-
-allUrbans :: IO ShpData
-allUrbans = readShpWithDbf "/home/cassowary/Projects/map/source-data/abs/1270055004_sos_2011_aust_shape/SOS_2011_AUST.shp"
-pickUrbans :: ShpData -> ([ShpRec], [ShpRec], [ShpRec])
-pickUrbans = pickRecords (("SOS_NAME11", "Major Urban"), ("SOS_NAME11", "Other Urban"), ("SOS_NAME11", "Bounded Locality"))
-
-
-allMunicipalities :: IO [ShpData]
-allMunicipalities = do
-    nsw <- readShpWithDbf "/home/cassowary/Projects/map/source-data/cth/NSWLGAPOLYGON/NSW_LGA_POLYGON_shp.shp"
-    vic <- readShpWithDbf "/home/cassowary/Projects/map/source-data/cth/VICLGAPOLYGON/VIC_LGA_POLYGON_shp.shp"
-    qld <- readShpWithDbf "/home/cassowary/Projects/map/source-data/cth/QLDLGAPOLYGON/QLD_LGA_POLYGON_shp.shp"
-    return $ [nsw, vic, qld]
-allLocalities :: IO [ShpData]
-allLocalities = do
-    nsw <- readShpWithDbf "/home/cassowary/Projects/map/source-data/cth/NSWLOCALITYPOLYGON/NSW_LOCALITY_POLYGON_shp.shp"
-    vic <- readShpWithDbf "/home/cassowary/Projects/map/source-data/cth/VICLOCALITYPOLYGON/VIC_LOCALITY_POLYGON_shp.shp"
-    qld <- readShpWithDbf "/home/cassowary/Projects/map/source-data/cth/QLDLOCALITYPOLYGON/QLD_LOCALITY_POLYGON_shp.shp"
-    return $ [nsw, vic, qld]
-
-readByState :: State -> String -> MunicipalityInState
-readByState Vic m = MuniVic $ read ("Vic" ++ m)
-readByState SA  m = MuniSA $ read ("SA" ++ m)
-readByState Qld m = MuniQld $ read ("Qld" ++ m)
-
-data MunicipalityInState =
-    MuniVic VictorianMunicipality
-    | MuniSA SouthAustralianMunicipality
-    | MuniQld QueenslandMunicipality
-   deriving (Show)
-
-getVictorianLocalities :: IO (Either String VictorianLocalities)
-getVictorianLocalities = fmap victorianLocalities readVictorianDb
-
-readVictorianDb :: IO BS.ByteString
-readVictorianDb = BS.readFile "/home/cassowary/Projects/map/source-data/vic/filesvicnamesplaces2014.csv"
-
--- pooy :: Municipality m => (m -> a) -> MunicipalityInState -> a
--- pooy f (MuniVic m) = f m
--- pooy f (MuniQld m) = f m
--- pooy f (MuniSA  m) = f m
-
-filterLocalities' :: MunicipalityInState -> Localities -> S.Set T.Text
-filterLocalities' (MuniVic m) = filterLocalities m
-filterLocalities' (MuniQld m) = filterLocalities m
-filterLocalities' (MuniSA  m) = filterLocalities m
-
-filterLocalities :: Municipality m => m -> Localities -> S.Set T.Text
-filterLocalities = flip localitiesByMunicipality
-
-filterVictorianMunicipalities :: Localities -> VictorianMunicipality -> S.Set T.Text
-filterVictorianMunicipalities = localitiesByMunicipality
-
-data GazetteTypeCode = LOCB | Other
-   deriving (Eq, Show)
-instance FromField GazetteTypeCode where
-   parseField "LOCB" = pure LOCB
-   parseField _ = pure Other
+-- makeBaseMap settings = BS.concat [initialiseMap settings
--- src/Map.hs
+++ src/Map.hs
@@ -1,19 +1,40 @@
+{-# LANGUAGE RankNTypes #-}
 module Map
 where
 
+import Data.List
 import Geometry.Shapefile.Types
 import qualified Shapefile2 as S
 import System.Process
 import qualified Data.Text as T
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as B8
+import Data.ByteString (ByteString)
+import qualified Data.Conduit.Combinators as CC
+import qualified Data.Conduit.Process as CP
+import Data.Conduit
+--import Data.Builder
+--import Data.Binary.Builder
+import Blaze.ByteString.Builder
+--import ClassyPrelude (traceM, trace, traceId)
+import Control.Monad.IO.Class
+import qualified Data.Vector as V
 
 class GMTOption a where
-    tshow :: a -> T.Text
+    tshow :: a -> String
 
 instance GMTOption T.Text where
-    tshow = id
+    tshow = T.unpack
 
 instance GMTOption RecBBox where
-    tshow b = T.concat $ map T.pack ["-R", show $ recXMin b, "/", show $ recXMax b, "/", show $ recYMin b, "/", show $ recYMax b]
+    tshow b = concat ["-R", 
+                      show $ recXMin b, 
+                      "/", 
+                      show $ recXMax b, 
+                      "/", 
+                      show $ recYMin b, 
+                      "/", 
+                      show $ recYMax b]
 
 data Orientation = Portrait | Landscape
 instance GMTOption Orientation where
@@ -47,12 +68,12 @@ data Pen
     | Water Color
     | Outline Width Color
 instance GMTOption Pen where
-    tshow (Solid color) = T.concat ["-G", colorToText color]
-    tshow (Water color) = T.concat ["-S", colorToText color]
-    tshow (Outline (Points pt) color) = T.concat ["-W", T.pack . show $ pt, "p,", colorToText color]
+    tshow (Solid color) = concat ["-G", colorToText color]
+    tshow (Water color) = concat ["-S", colorToText color]
+    tshow (Outline (Points pt) color) = concat ["-W", show pt, "p,", colorToText color]
 
-colorToText :: Color -> T.Text
-colorToText (Color red green blue) = T.intercalate "/" (map (T.pack . show) [red, green, blue])
+colorToText :: Color -> String
+colorToText (Color red green blue) = intercalate "/" (map show [red, green, blue])
 
 -- settingsToArgs :: GMTSettings -> [T.Text]
 -- settingsToArgs settings = [orientation settings, projection settings, penToText . pen $ settings]
@@ -60,14 +81,18 @@ colorToText (Color red green blue) = T.intercalate "/" (map (T.pack . show) [red
 -- withPen :: GMTPen -> GMTSettings
 -- withPen newColor = GMTSettings { orientation = "-P", projection = "-JM144/37/15c", pen = newColor }
 
-pointToPstPoint :: S.Point -> T.Text
-pointToPstPoint (easting, westing) = T.pack $ show easting ++ " " ++ show westing
+pointToPstPoint :: S.Point -> ByteString
+pointToPstPoint (easting, westing) = B8.pack $ show easting ++ " " ++ show westing
 
-polygonToPstPoints :: [S.Point] -> T.Text
-polygonToPstPoints pts = T.unlines $ map pointToPstPoint pts
+polygonToPstPoints :: [S.Point] -> ByteString
+polygonToPstPoints pts = BS.intercalate "\n" (map pointToPstPoint pts)
 
-polygonsToPstPoints :: [[S.Point]] -> T.Text
-polygonsToPstPoints pts = T.intercalate ">\n" (map polygonToPstPoints pts)
+polygonsToPstPoints :: [[S.Point]] -> ByteString
+polygonsToPstPoints pts = f
+   where f = BS.intercalate "\n>\n" ("":(polygonToPstPoints <$> pts))
+
+polygonsToPstPointsConduit :: Conduit [S.Point] IO ByteString
+polygonsToPstPointsConduit = CC.map polygonToPstPoints =$= CC.intersperse ">\n"
 
 --drawMap :: Settings -> [(Pen, Int, [[S.Point]])] -> IO T.Text
 --drawMap settings points = do
@@ -75,20 +100,20 @@ polygonsToPstPoints pts = T.intercalate ">\n" (map polygonToPstPoints pts)
         -- <|> (foldr (<|>) empty (map (mapPoints settings) points))
         -- <|> closeMap settings
 
-initialiseMap :: Settings -> IO T.Text
+initialiseMap :: Settings -> IO ByteString
 initialiseMap settings
-    = inproc "gmt" ["psxy", "-A", "-T", setting projection, setting boundingBox, "-K", setting orientation] T.empty
+    = inproc "gmt" ["psxy", "-A", "-T", setting projection, setting boundingBox, "-K", setting orientation] BS.empty
     where
         setting getter = tshow . getter $ settings
 
-closeMap :: Settings -> IO T.Text
+closeMap :: Settings -> IO ByteString
 closeMap settings
-    = inproc "gmt" ["psxy", "-A", "-T", setting projection, setting boundingBox, "-O", setting orientation] T.empty
+    = inproc "gmt" ["psxy", "-A", "-T", setting projection, setting boundingBox, "-O", setting orientation] BS.empty
     where
         setting getter = tshow . getter $ settings
 
-mapPoints :: Settings -> (Pen, Int, [[S.Point]]) -> IO T.Text
-mapPoints settings (pen, transparency, points) = inproc "gmt" params (polygonsToPstPoints points)
+mapPointsParams :: (Show s, GMTOption o) => Settings -> (o, s) -> [String]
+mapPointsParams settings (pen, transparency) = params
     where
         setting getter = tshow . getter $ settings
         params = [
@@ -99,13 +124,40 @@ mapPoints settings (pen, transparency, points) = inproc "gmt" params (polygonsTo
             "-K",
             "-O",
             setting orientation,
-            T.pack $ "-t" ++ show transparency
+            "-t" ++ show transparency
             ]
 
-inproc :: String -> [T.Text] -> T.Text -> IO T.Text
-inproc a b c = fmap T.pack (readProcess a (fmap T.unpack b) (T.unpack c))
+mapPoints3 :: Settings -> Pen -> Int -> Conduit [[S.Point]] IO ByteString
+mapPoints3 settings pen transparency = CC.conduitVector 200 =$= awaitForever go
+	where
+		go points = (liftIO $ mapPoints1 settings (pen, transparency, concat $ V.toList points)) >>= yield
+--mapPoints4 settings pen transparency = do
+   --blah <- await
+   --traceM (show blah)
+   --case blah of
+      --Nothing -> return ()
+      --Just points -> (liftIO $ mapPoints1 settings (pen, transparency, [points])) >>= yield
 
-mapCoast :: Settings -> IO T.Text
+-- mapPoints2 ::Settings -> Pen -> Int -> Producer IO [S.Point] -> IO ByteString
+-- mapPoints2 settings pen transparency points
+--    = do
+--     (a, builder, c ) <- CP.sourceProcessWithStreams 
+--        (proc "gmt" (mapPointsParams settings (pen, transparency)))
+--        (points =$= polygonsToPstPointsConduit)
+--        CC.sinkBuilder
+--        CC.sinkBuilder
+--     traceM $ show a
+--     traceM $ show $ toByteString builder
+--     traceM $ show $ toByteString c
+--     return $ toByteString builder
+
+-- mapPoints1 :: Settings -> (Pen, Int, [[S.Point]]) -> IO ByteString
+mapPoints1 settings (pen, transparency, points) = inproc "gmt" (mapPointsParams settings (pen, transparency)) (polygonsToPstPoints points)
+
+inproc :: String -> [String] -> ByteString -> IO ByteString
+inproc a b c = B8.pack <$> readProcess a b (B8.unpack c)
+
+mapCoast :: Settings -> IO ByteString
 mapCoast settings = inproc "gmt" [
     "pscoast",
     setting projection,
@@ -119,6 +171,6 @@ mapCoast settings = inproc "gmt" [
     "-X2.2c",
     setting orientation,
     "-K"
-    ] T.empty
+    ] BS.empty
     where
         setting getter = tshow . getter $ settings
